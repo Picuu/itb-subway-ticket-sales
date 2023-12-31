38e8774c59f352de58656c53d3557e022e3a4fb0
alanteixido <alan.teixido.7e7@itb.cat>
2023-12-31 10:49:18 +0100

    Commit amb FuncionsEntrada.kt, Utilities.kt i ConsoleColors.kt

---
+++ src/main/kotlin/FuncionsEntrada.kt
@@ -0,0 +1,66 @@
+import org.example.RED
+import org.example.RESET
+import java.util.Scanner
+
+fun llegirInt(mensaje:String,intMin:Int,intMax:Int):Int{
+    val scan=Scanner(System.`in`)
+    var valorInt=0
+    var valorCorrecto:Boolean
+    do {
+        print("$mensaje ")
+        valorCorrecto=scan.hasNextInt()
+        if (!valorCorrecto){
+            println(RED+"ERROR: introduce un número entero"+RESET)
+        }else{
+            valorInt=scan.nextInt()
+            if (valorInt<intMin || valorInt>intMax){
+                valorCorrecto=false
+                println(RED+"ERROR: Introduce un número entre los valores requeridos"+RESET)
+            }
+        }
+        scan.nextLine()
+    }while (!valorCorrecto)
+    return valorInt
+}
+
+fun llegirLong(mensaje:String,longMin:Long): Long {
+    val scan=Scanner(System.`in`)
+    var valorLong:Long=0
+    var valorCorrecto:Boolean
+    do {
+        print("$mensaje ")
+        valorCorrecto=scan.hasNextLong()
+        if (!valorCorrecto){
+            println( RED +"ERROR: introduce un número entero"+ RESET)
+        }else{
+            valorLong= scan.nextLong()
+            if (valorLong<longMin){
+                valorCorrecto=false
+                println(RED+"ERROR: Introduce un número mayor a 0"+RESET)
+            }
+        }
+        scan.nextLine()
+    }while (!valorCorrecto)
+    return valorLong
+}
+
+fun llegirDouble(mensaje:String,intMin:Double,intMax:Double):Double{
+    val scan=Scanner(System.`in`)
+    var valorDouble=0.0
+    var valorCorrecto:Boolean
+    do {
+        print("$mensaje ")
+        valorCorrecto=scan.hasNextInt()
+        if (!valorCorrecto){
+            println(RED+"ERROR: introduce un número decimal"+RESET)
+        }else{
+            valorDouble=scan.nextDouble()
+            if (valorDouble<intMin || valorDouble>intMax){
+                valorCorrecto=false
+                println(RED+"ERROR: Introduce un número entre los valores requeridos"+RESET)
+            }
+        }
+        scan.nextLine()
+    }while (!valorCorrecto)
+    return valorDouble
+}
